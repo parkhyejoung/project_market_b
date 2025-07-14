a5c33a8955ed4ecf6796db459de2fcdedba429ed
hyejoung <qkrgpwjd1996@gmail.com>
2025-07-14 18:01:55 +0900

    선택자 수정

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
-const slide = new Swiper('.swiper-container',{
+const slide = new Swiper('.swiper-container .swiper-wrapper',{
     navigation: {
+        loop: true,
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
